07a9e81f576d7ba2c16229ad611f8f80bc5ad669
ahacad <ahacad@126.com>
2021-07-04 10:38:59 +0800

    fix(tests): add dependencies for tess.js

--- tests/test.js
+++ tests/test.js
@@ -1,23 +1,27 @@
-var vfile = require('to-vfile');
-var report = require('vfile-reporter');
-var unified = require('unified');
-var details = require('/home/ir1d/Documents/repo/remark-details/index.js');
-var markdown = require('remark-parse');
-var magic = require('./index.js');
-var remark2rehype = require('remark-rehype');
-var html = require('rehype-stringify');
+const test = require('ava');
 
-unified()
-  .use(markdown)
-  .use(details)
-  .use(remark2rehype)
-  .use(magic)
-  .use(html)
-  .process(vfile.readSync('tests/example.md'), function (err, file) {
-    console.error(report(err || file));
-    console.log(String(file));
-  });
+const vfile = require('to-vfile');
+const report = require('vfile-reporter');
+const unified = require('unified');
+const details = require('remark-details');
+const markdown = require('remark-parse');
+const magic = require('../index.js');
+const remark2rehype = require('remark-rehype');
+const html = require('rehype-stringify');
+
+test('main', (t) => {
+  unified()
+    .use(markdown)
+    .use(details)
+    .use(remark2rehype)
+    .use(magic)
+    .use(html)
+    .process(vfile.readSync('tests/example.md'), function (err, file) {
+      console.error(report(err || file));
+      console.log(String(file));
+    });
+  t.pass();
+});
 
 // to-vfile vfile-reporter unified remark-parse remark-rehype
 // rehype-stringify
-
