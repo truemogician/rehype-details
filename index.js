2d11cd651c86a7ba418052274a3706f3f9ae841b
ahacad <ahacad@126.com>
2021-07-04 10:35:10 +0800

    chore(index): format index, remove redundant codes

--- index.js
+++ index.js
@@ -1,79 +1,66 @@
-const visit = require('unist-util-visit')
-const unified = require('unified')
-const parse = require('rehype-parse')
-const toHTML = require('hast-util-to-html')
-// const hastToString = require('hast-util-to-string')
+const visit = require('unist-util-visit');
+const unified = require('unified');
+const parse = require('rehype-parse');
+const toHTML = require('hast-util-to-html');
 
-module.exports = rehypeDetails
-
-const assign = Object.assign
-
-const parseHtml = unified().use(parse, {fragment: true, position: false})
-
-const source = 'rehype-details'
+const parseHtml = unified().use(parse, { fragment: true, position: false });
 
 function rehypeDetails(options) {
-  const opts = options || {}
-  const throwOnError = opts.throwOnError || false
+  const opts = options || {};
 
-  return transformDetails
+  return transformDetails;
 
   function transformDetails(tree, file) {
-    // var Compiler = this.Compiler;
-    // var visitors = Compiler.prototype.visitors;
-    // console.log(Compiler)
-    visit(tree, 'element', visitor)
+    visit(tree, 'element', visitor);
 
     function visitor(node, index, parent) {
-      const className = node.properties.className || []
+      const className = node.properties.className || [];
       if (node.tagName !== 'div' || !className.includes('details')) {
         return;
       }
       // console.log(node)
-  
-      let header = node.properties.header || ""
-      let icon = 'note'
-      let display = 0
-      let value = node.properties.summary || "<p>Details</p>"
-      
-      if (header.length > 0)
-        header = header.replace('\n', '')
 
-        // const arr = header.split(value)
-        if (header.indexOf('!!!') > -1 || header.substr(0, 4).indexOf('+') > -1) display = 1
-        
+      let header = node.properties.header || '';
+      let icon = 'note';
+      let display = 0;
+      let value = node.properties.summary || '<p>Details</p>';
+
+      if (header.length > 0) header = header.replace('\n', '');
+
+      // const arr = header.split(value)
+      if (header.indexOf('!!!') > -1 || header.substr(0, 4).indexOf('+') > -1)
+        display = 1;
+
       //   // const title = arr[1]
-        if (value.indexOf('warning') > -1) icon = 'warning'
+      if (value.indexOf('warning') > -1) icon = 'warning';
       //   // value = value.replace(icon, '').trim()
       //   // console.log(`${title}, ${header}, ${value}, ${display}`)
       //   // value = value.replace(/"+$/, "").replace(/^"+/, "").trim();
       // }
 
-
-
-      const ch = node.children
+      const ch = node.children;
       // console.log(header)
       // console.log(toHTML(node))
-      let classes = `details-${icon}` + (display?` details-open`:``)
+      let classes = `details-${icon}` + (display ? ` details-open` : ``);
       // console.log(classes)
-      let starter = `<details class="${classes}">`
+      let starter = `<details class="${classes}">`;
       if (value && value.length > 0) {
-        value = value.trim()
-        starter += `<summary class="summary-note">${value}</summary>`
+        value = value.trim();
+        starter += `<summary class="summary-note">${value}</summary>`;
       }
-      let result = ''
-      let ending = `</details>`
-      for (let cur of ch) result += toHTML(cur)
-//       node.children
+      let result = '';
+      let ending = `</details>`;
+      for (let cur of ch) result += toHTML(cur);
+      //       node.children
 
-      
-// {/* <summary>Details</summary> */}
-
+      // {/* <summary>Details</summary> */}
 
       // console.log((ch.length))
       // console.log((starter + result + ending))
       // node.children = parseHtml.parse(result).children
-      node.children = parseHtml.parse(starter + result + ending).children
+      node.children = parseHtml.parse(starter + result + ending).children;
     }
   }
 }
+
+module.exports = rehypeDetails;
