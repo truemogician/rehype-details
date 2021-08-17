2136f56a4fb8f515f6d16599b7e1adce08482046
true_mogician <925269713@qq.com>
2021-08-17 23:41:24 +0800

    refactor

--- dist/index.js
+++ dist/index.js
@@ -7,17 +7,13 @@ var _hastscript = require("hastscript");
 
 var _unistUtilVisit = require("unist-util-visit");
 
-function rehypeDetails(options) {
-  return transform;
-
-  function transform(tree) {
-    (0, _unistUtilVisit.visit)(tree, ['detailsContainer', 'detailsContainerSummary'], ondetails);
-  }
-
-  function ondetails(node) {
-    var data = node.data || (node.data = {});
-    var hast = (0, _hastscript.h)(node.name, node.attributes);
-    data.hName = hast.tagName;
-    data.hProperties = hast.properties;
-  }
+function rehypeDetails() {
+  return tree => {
+    (0, _unistUtilVisit.visit)(tree, ['detailsContainer', 'detailsContainerSummary'], node => {
+      var data = node.data || (node.data = {});
+      var hast = (0, _hastscript.h)(node.name, node.attributes);
+      data.hName = hast.tagName;
+      data.hProperties = hast.properties;
+    });
+  };
 }
--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 import { h } from 'hastscript';
 import { visit } from 'unist-util-visit';
 
-export default function rehypeDetails(options) {
-  return transform;
-
-  function transform(tree) {
-    visit(tree, ['detailsContainer', 'detailsContainerSummary'], ondetails);
-  }
-
-  function ondetails(node) {
-    var data = node.data || (node.data = {});
-    var hast = h(node.name, node.attributes);
-
-    data.hName = hast.tagName;
-    data.hProperties = hast.properties;
-  }
+export default function rehypeDetails() {
+  return (tree) => {
+    visit(tree, ['detailsContainer', 'detailsContainerSummary'], (node) => {
+      var data = node.data || (node.data = {});
+      var hast = h(node.name, node.attributes);
+      data.hName = hast.tagName;
+      data.hProperties = hast.properties;
+    });
+  };
 }
