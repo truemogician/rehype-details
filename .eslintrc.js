769942f71c58da877a04284e2755c365969ebb45
true_mogician <925269713@qq.com>
2021-08-17 23:40:31 +0800

    style: clean up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   env: { es6: true, node: true },
   parserOptions: { ecmaVersion: 8 },
 };
--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,7 +1,5 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import { execSync } from 'child_process';
 import fs from 'fs-extra';
-import { $ } from 'zx';
 
 execSync('yarn babel index.js --out-dir dist', {
   stdio: 'inherit',
