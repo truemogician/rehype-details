36bcabc194e686c14c0e9911223a9824e7534523
true_mogician <925269713@qq.com>
2021-08-19 10:55:50 +0800

    fix: build script

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -2,6 +2,9 @@ import { execSync } from 'child_process';
 import fs from 'fs-extra';
 
 execSync('tsc', { stdio: 'inherit' });
-execSync('yarn babel index.js --out-dir dist', { stdio: 'inherit' });
-fs.copyFileSync('index.js', 'dist/index.mjs');
-fs.renameSync('dist/index.js', 'dist/index.cjs');
+const cjs = 'lib/index.cjs';
+if (fs.existsSync(cjs))
+	fs.removeSync(cjs)
+execSync(`yarn babel index.js -o ${cjs}`, { stdio: 'inherit' });
+fs.moveSync("index.d.ts", "lib/index.d.ts");
+fs.moveSync('index.js', "lib/index.mjs");
